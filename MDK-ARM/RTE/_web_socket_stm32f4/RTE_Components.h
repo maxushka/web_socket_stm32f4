1c9ddba7a4b25fb483ae9bed80bab789cb9b7e96
MAXPC <lll>
2021-02-13 01:09:32 +0300

    Add Keil uVision Project

---
+++ MDK-ARM/RTE/_web_socket_stm32f4/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'web_socket_stm32f4' 
+ * Target:  'web_socket_stm32f4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
